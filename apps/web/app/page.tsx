7a9cbd454ecdf5a5ed61995fd5210ac117ee319a
swaparup36 <swaparup36@gmail.com>
2025-03-31 00:39:07 +0530

    changes in fe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home () {
 
   return (
     <div>
-      {user?.username}
-      {user?.password}
+      username: {user?.username}
+      password: {user?.password}
     </div>
   )
 }
